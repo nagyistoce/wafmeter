5301ab9f93d8ab1adcf33c18d262ff3f0d42ab33
cseyve@free.fr <cseyve@free.fr@81378b52-85ef-11de-bdfc-c94c2ab25652>
2009-08-14 14:05:49 +0000

    Added iphone prototype

--- trunk/src/wafmainwindow.cpp
+++ trunk/src/wafmainwindow.cpp
@@ -4,27 +4,27 @@
 #include <highgui.h>
 
 WAFMainWindow::WAFMainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::WAFMainWindow)
+	: QMainWindow(parent), ui(new Ui::WAFMainWindow)
 {
 	char home[512]=".";
 	if(getenv("HOME")) {
 		strcpy(home, getenv("HOME"));
 	}
 	m_path = QString(home);
-    ui->setupUi(this);
-	
+	ui->setupUi(this);
+
 	QString filename=":/qss/WAFMeter.qss";
 	QFile file(filename);
 	file.open(QFile::ReadOnly);
 	QString styleSheet = QLatin1String(file.readAll());
-	setStyleSheet(styleSheet);
+	//setStyleSheet(styleSheet);
 
 
 }
 
 WAFMainWindow::~WAFMainWindow()
 {
-    delete ui;
+	delete ui;
 }
 
 void WAFMainWindow::on_fileButton_pressed() {
@@ -60,7 +60,17 @@ void WAFMainWindow::on_fileButton_pressed() {
 	ui->wafProgressBar->setValue(100.f * waf.waf_factor);
 	ui->colorProgressBar->setValue(100.f * waf.color_factor);
 	ui->contourProgressBar->setValue(100.f * waf.contour_factor);
-
+/*
+	QString dialname=":/icons/Dialer.png";
+	QImage pixmap(dialname);
+	pixmap = pixmap.convertToFormat(QImage::Format_Indexed8);
+	pixmap.setNumColors(256);
+	for(int col = 0; col < 256; col++)
+		pixmap.setColor(col, qRgb(col, col, col));
+	int arrow = (int)(255.f * waf.waf_factor);
+	pixmap.setColor(arrow, qRgb(255, 0, 0));
+	ui->dialLabel->setPixmap(QPixmap(pixmap));
+*/
 	cvReleaseImage(&iplImage);
 }
 
